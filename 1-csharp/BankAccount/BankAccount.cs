549efa2d251a8c4caa6afdb2835ad7d0e08a7e84
Josh Bertrand <joshuabertrand@protonmail.com>
2020-06-17 15:06:56 -0400

    Exercise creating a bank account application in CSharp.

---
+++ 1-csharp/BankAccount/BankAccount.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+using classes;
+
+//https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/intro-to-csharp/introduction-to-classes
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        public string number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private static int accountNumberSeed = 1234567890;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient funds for this transaction");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.number} was created for {account.Owner} with ${account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance ");
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exceptions caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
