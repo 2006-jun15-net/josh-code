28c9e7608444b3628717ed4319c1ee8918218919
Josh Bertrand <joshuabertrand@protonmail.com>
2020-06-26 12:06:34 -0400

    Practice using C# ORM to edit database tables

--- 2-sql/ChinookApp/ChinookApp/Program.cs
+++ 2-sql/ChinookApp/ChinookApp/Program.cs
@@ -31,23 +31,85 @@ namespace ChinookApp
             Remember when generating code, go to the <db>Context.cs file, and delete the OnConfig class due to it having the password for the db included in the auto generation
              */
 
-            
+            DisplayData();
 
-            
+            Console.WriteLine("\nAdding some data...");
+            AddSomeDataFromUserInput();
+
+            DisplayData();
+
+            Console.WriteLine("\nUpdating some data...");
+            UpdateSomeData();
+
+            DisplayData();
+
+            Console.WriteLine("\nDeleting some data...");
+            DeleteSomeData();
+
+            DisplayData();
+
+
+        }
+
+        private static void DeleteSomeData()
+        {
+            using var context = new ChinookContext(options);
+
+            // In order to properly find and delete the data, a select query will have to be made to find it
+            var firedEmployee = context.Exemployee
+                .Where(e => e.Id > 1)
+                .ToList();
+
+            foreach (var emp in firedEmployee)
+            {
+                context.Remove(emp);
+            }
+
+            context.SaveChanges();
+        }
+
+        private static void UpdateSomeData()
+        {
+            using var context = new ChinookContext(options);
+
+            var employee = context.Exemployee.First(a => a.FirstName == "Amanda");
+            employee.FirstName = "Ellen";
+            context.SaveChanges();
+        }
+
+        private static void AddSomeDataFromUserInput()
+        {
+
+            //before allowing userdata to be implemented into the DB, you would want to add some input validation to ensure that the user is not entering anything malicious.
+            using var context = new ChinookContext(options);
+
+            //This will throw an error currently due to the Id not auto-incrementing in the DB itself unless Id is changed to something not currently in the Db. 
+            Exemployee emp1 = new Exemployee {Id = 3, FirstName = "Amanda", LastName = "Ripley", Ssn = "1234447809", DeptId = 2 };
+
+            var newEmployee = context.Add(emp1);//multiple entries can be added to the db by using the AddRange() method and supplying a Collection
+            context.SaveChanges();
+            Console.WriteLine("New Employee has been added...");
         }
 
         public static void DisplayData()
         {
             using var context = new ChinookContext(options);
 
-            var salespeople = context.Employee
-                .Where(e => e.Title.Contains("sales"))
-                .ToList();
-
-            foreach (var person in salespeople)
+            var employees = context.Exemployee.ToList();
+            Console.WriteLine("-----Displaying all employees-----");
+            foreach (var emp in employees)
             {
-                Console.WriteLine($"\t{person.Title} - {person.FirstName} {person.LastName}");
+                Console.WriteLine($"\t{emp.FirstName} {emp.LastName}");
             }
+
+            //var salespeople = context.Employee
+            //    .Where(e => e.Title.Contains("sales"))
+            //    .ToList();
+
+            //foreach (var person in salespeople)
+            //{
+            //    Console.WriteLine($"\t{person.Title} - {person.FirstName} {person.LastName}");
+            //}
         }
     }
 }
